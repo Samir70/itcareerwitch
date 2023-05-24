5c1d1d8fcbb97a9f0f8a046d326f8ddc03f7b539
Samir70 <sbetmouni@talktalk.net>
2023-05-24 17:06:51 +0100

    python module 2

---
+++ Python/hello.py
@@ -0,0 +1,6 @@
+"""Greet func, that defaults to 'hello world' """
+def greet(name="World"):
+    """function greet takes one variable (defaults to 'World'). Prints 'Hello <var>'"""
+    print("Hello "+name)
+
+greet("Samir")
---
+++ Python/tests_02.py
@@ -0,0 +1,14 @@
+"""Tests at the end of module 02"""
+
+x = 15
+print(type(x))
+
+y = 6.36272
+print(type(y))
+
+b = True
+print(b)
+
+# concat 3 strings
+[s, t, u] = ["One", "Two", "Three"]
+print(s + t + u)
