df6de80d3a3fbeb90b6499ea38b206027b1b1c60
Samir70 <sbetmouni@talktalk.net>
2023-06-01 12:33:55 +0100

    custom modules

---
+++ Python/m20_hello.py
@@ -0,0 +1,5 @@
+'''Hello function'''
+
+def greet(name):
+    '''function that greets people by name'''
+    print(F"Hello, {name}. How are you?")
---
+++ Python/m20_modules.py
@@ -0,0 +1,6 @@
+'''importing custom module'''
+from m20_hello import greet
+# alt is import m20_hello and call as m20_hello.greet()
+# or use import m20_hello as hello and call as hello.greet()
+
+greet("World")
