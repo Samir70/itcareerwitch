c66e3c239a8fda8a39a755d44d191d194a963bee
Samir70 <sbetmouni@talktalk.net>
2023-06-01 13:30:33 +0100

    datetime, os.stat, os.getcwd

---
+++ Python/m25_datetime.py
@@ -0,0 +1,28 @@
+'''datetime'''
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+today = date.today()
+date_time = datetime.now()
+
+print("Today's date is: ", today)
+print("Can you be more specific please? ", date_time)
+print("Break it down....")
+print("The hour ", date_time.hour)
+print("The minute ", date_time.minute)
+print("The seconds ", date_time.second)
+
+#Working directory
+print("\n")
+import os
+
+dirpath = os.getcwd()
+print("Your current working directory is: " + dirpath)
+
+foldername = os.path.basename(dirpath)
+print("The specific folder is: " + foldername)
+
+print("=========================")
+import os
+print(os.stat("m25_datetime.py"))
